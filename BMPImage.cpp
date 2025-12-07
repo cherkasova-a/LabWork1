51ffb386bb463be889252b8016277625fbc02b81
Anastasia Cherkasova <st140594@student.spbu.ru>
2025-12-07 13:06:32 +0300

    correct mistakes

--- BMPImage.cpp
+++ BMPImage.cpp
@@ -3,9 +3,17 @@
 *st140594@student.spbu.ru
 *LabWork1
 */
+/*
+*Anastasia Cherkasova
+*st140594@student.spbu.ru
+*LabWork1
+*/
 #include "BMPImage.h"
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
 
 bool BMPImage::load(const std::string& filename) {
     std::ifstream file(filename, std::ios::binary);
@@ -39,46 +47,57 @@ bool BMPImage::save(const std::string& filename) {
     file.close();
     return true;
 }
+
 void BMPImage::rotate90clockwise() {
-    int rowSize = ((24 * width + 31) / 32) * 4;
+    int oldRowSize = ((24 * width + 31) / 32) * 4;
     int newWidth = height;
     int newHeight = width;
-    std::vector<uint8_t> newPixels(newHeight * ((24*newWidth+31)/32*4));
+    int newRowSize = ((24 * newWidth + 31) / 32) * 4;
+
+    std::vector<uint8_t> newPixels(newRowSize * newHeight, 0);
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            int srcIndex = y * rowSize + x * 3;
-            int destRowSize = ((24 * newWidth + 31) / 32) * 4;
-            int destIndex = x * destRowSize + (newWidth - 1 - y) * 3;
+            int srcIndex = y * oldRowSize + x * 3;
+            int destIndex = x * newRowSize + (newWidth - 1 - y) * 3;
             newPixels[destIndex] = pixelData[srcIndex];
-            newPixels[destIndex+1] = pixelData[srcIndex+1];
-            newPixels[destIndex+2] = pixelData[srcIndex+2];
+            newPixels[destIndex + 1] = pixelData[srcIndex + 1];
+            newPixels[destIndex + 2] = pixelData[srcIndex + 2];
         }
     }
 
-    std::swap(width, height);
+    width = newWidth;
+    height = newHeight;
     pixelData = std::move(newPixels);
+
+    *reinterpret_cast<int*>(&dibHeader[4]) = width;
+    *reinterpret_cast<int*>(&dibHeader[8]) = height;
 }
 
 void BMPImage::rotate90counter() {
-    int rowSize = ((24 * width + 31) / 32) * 4;
+    int oldRowSize = ((24 * width + 31) / 32) * 4;
     int newWidth = height;
     int newHeight = width;
-    std::vector<uint8_t> newPixels(newHeight * ((24*newWidth+31)/32*4));
+    int newRowSize = ((24 * newWidth + 31) / 32) * 4;
+
+    std::vector<uint8_t> newPixels(newRowSize * newHeight, 0);
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            int srcIndex = y * rowSize + x * 3;
-            int destRowSize = ((24 * newWidth + 31) / 32) * 4;
-            int destIndex = (newHeight - 1 - x) * destRowSize + y * 3;
+            int srcIndex = y * oldRowSize + x * 3;
+            int destIndex = (newHeight - 1 - x) * newRowSize + y * 3;
             newPixels[destIndex] = pixelData[srcIndex];
-            newPixels[destIndex+1] = pixelData[srcIndex+1];
-            newPixels[destIndex+2] = pixelData[srcIndex+2];
+            newPixels[destIndex + 1] = pixelData[srcIndex + 1];
+            newPixels[destIndex + 2] = pixelData[srcIndex + 2];
         }
     }
 
-    std::swap(width, height);
+    width = newWidth;
+    height = newHeight;
     pixelData = std::move(newPixels);
+
+    *reinterpret_cast<int*>(&dibHeader[4]) = width;
+    *reinterpret_cast<int*>(&dibHeader[8]) = height;
 }
 
 void BMPImage::applyGaussian3x3() {
@@ -86,28 +105,28 @@ void BMPImage::applyGaussian3x3() {
     std::vector<uint8_t> newPixels = pixelData;
 
     int kernel[3][3] = {{1,2,1},{2,4,2},{1,2,1}};
+    int kernelSum = 16;
 
-    for (int y = 1; y < height-1; ++y) {
-        for (int x = 1; x < width-1; ++x) {
-            int sumR=0, sumG=0, sumB=0;
+    for (int y = 1; y < height - 1; ++y) {
+        for (int x = 1; x < width - 1; ++x) {
+            int sumR = 0, sumG = 0, sumB = 0;
 
-            for (int ky=-1; ky<=1; ++ky) {
-                for (int kx=-1; kx<=1; ++kx) {
-                    int px = x+kx;
-                    int py = y+ky;
-                    int idx = py*rowSize + px*3;
-                    sumB += pixelData[idx] * kernel[ky+1][kx+1];
-                    sumG += pixelData[idx+1] * kernel[ky+1][kx+1];
-                    sumR += pixelData[idx+2] * kernel[ky+1][kx+1];
+            for (int ky = -1; ky <= 1; ++ky) {
+                for (int kx = -1; kx <= 1; ++kx) {
+                    int idx = (y + ky) * rowSize + (x + kx) * 3;
+                    sumB += pixelData[idx] * kernel[ky + 1][kx + 1];
+                    sumG += pixelData[idx + 1] * kernel[ky + 1][kx + 1];
+                    sumR += pixelData[idx + 2] * kernel[ky + 1][kx + 1];
                 }
             }
 
-            int dstIdx = y*rowSize + x*3;
-            newPixels[dstIdx]   = sumB / 16;
-            newPixels[dstIdx+1] = sumG / 16;
-            newPixels[dstIdx+2] = sumR / 16;
+            int dstIdx = y * rowSize + x * 3;
+            newPixels[dstIdx] = sumB / kernelSum;
+            newPixels[dstIdx + 1] = sumG / kernelSum;
+            newPixels[dstIdx + 2] = sumR / kernelSum;
         }
     }
 
     pixelData = std::move(newPixels);
 }
+
