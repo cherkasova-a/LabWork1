d06add52f952167f5466bc58e69d461949a1801d
Anastasia Cherkasova <st140594@student.spbu.ru>
2025-12-14 22:19:32 +0300

    push

--- BMPImage.cpp
+++ BMPImage.cpp
@@ -1,13 +1,17 @@
+/*
+*Anastasia Cherkasova
+*st140594@student.spbu.ru
+*LabWork1
+*/
 #include "BMPImage.h"
 #include "BMPConstants.h"
 #include <fstream>
-#include <stdexcept>
 #include <vector>
+#include <stdexcept>
+#include <cstdint>
 #include <algorithm>
-#include <cmath>
 
-bool BMPImage::load(const std::string& filename)
-{
+bool BMPImage::load(const std::string& filename) {
     std::ifstream file(filename, std::ios::binary);
     if (!file) return false;
 
@@ -16,59 +20,56 @@ bool BMPImage::load(const std::string& filename)
     if (bmpHeader[0] != 'B' || bmpHeader[1] != 'M')
         throw std::runtime_error("Not a BMP file");
 
+    uint32_t pixelOffset = *reinterpret_cast<uint32_t*>(&bmpHeader[10]);
     uint32_t dibSize = 0;
     file.read(reinterpret_cast<char*>(&dibSize), sizeof(uint32_t));
-    if (dibSize < DIB_HEADER_SIZE)
-        throw std::runtime_error("Unsupported DIB header size");
 
-    file.seekg(BMP_HEADER_SIZE, std::ios::beg);
     dibHeader.resize(dibSize);
-    file.read(reinterpret_cast<char*>(dibHeader.data()), dibSize);
+    *reinterpret_cast<uint32_t*>(dibHeader.data()) = dibSize;
+    file.read(reinterpret_cast<char*>(dibHeader.data() + 4), dibSize - 4);
 
-    width = read_s32_le(&dibHeader[4]);
-    int32_t rawHeight = read_s32_le(&dibHeader[8]);
+    width = *reinterpret_cast<int32_t*>(&dibHeader[4]);
+    int32_t rawHeight = *reinterpret_cast<int32_t*>(&dibHeader[8]);
+    originalTopDown = rawHeight < 0;
     height = std::abs(rawHeight);
-    originalTopDown = (rawHeight < 0);
 
-    uint16_t planes = read_u16_le(&dibHeader[12]);
-    uint16_t bpp = read_u16_le(&dibHeader[14]);
-    uint32_t compression = read_u32_le(&dibHeader[16]);
+    uint16_t planes = *reinterpret_cast<uint16_t*>(&dibHeader[12]);
+    uint16_t bpp = *reinterpret_cast<uint16_t*>(&dibHeader[14]);
+    uint32_t compression = *reinterpret_cast<uint32_t*>(&dibHeader[16]);
 
-    if (planes != 1 || bpp != BYTES_PER_PIXEL_24 * 8 || compression != 0)
+    if (planes != 1 || bpp != 24 || compression != 0)
         throw std::runtime_error("Only uncompressed 24bpp BMP supported");
 
-    uint32_t pixelOffset = read_u32_le(&bmpHeader[10]);
     int rowSize = calculateRowSize(width);
     pixelData.resize(rowSize * height);
-
     file.seekg(pixelOffset, std::ios::beg);
     file.read(reinterpret_cast<char*>(pixelData.data()), pixelData.size());
-    file.close();
 
     if (originalTopDown) {
-        for (int y = 0; y < height / 2; ++y) {
-            std::swap_ranges(
-                pixelData.begin() + y * rowSize,
-                pixelData.begin() + (y + 1) * rowSize,
-                pixelData.begin() + (height - 1 - y) * rowSize
-            );
+        std::vector<uint8_t> tmp(pixelData.size());
+        int rowBytes = rowSize;
+        for (int y = 0; y < height; ++y) {
+            std::copy_n(&pixelData[y * rowBytes], rowBytes, &tmp[(height - 1 - y) * rowBytes]);
         }
+        pixelData.swap(tmp);
     }
 
+    file.close();
     return true;
 }
 
-bool BMPImage::save(const std::string& filename)
-{
+bool BMPImage::save(const std::string& filename) {
     std::ofstream file(filename, std::ios::binary);
     if (!file) return false;
 
     file.write(reinterpret_cast<char*>(bmpHeader.data()), bmpHeader.size());
     file.write(reinterpret_cast<char*>(dibHeader.data()), dibHeader.size());
     file.write(reinterpret_cast<char*>(pixelData.data()), pixelData.size());
+
     file.close();
     return true;
 }
+
 
 void BMPImage::rotate90clockwise()
 {
